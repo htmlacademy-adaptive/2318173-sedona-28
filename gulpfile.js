a1189f45d84ff58825d5f2cdfdb661c035a55dbb
Ганина Анастасия <122857367+AnastasiyaGanina@users.noreply.github.com>
2023-05-09 09:52:13 +0300

    Начало сборки

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
 import autoprefixer from 'autoprefixer';
+import del from 'del';
 import browser from 'browser-sync';
 
 // Styles
@@ -12,18 +20,70 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso ()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename ('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+//HTML
+export const minify = () => {
+  return gulp.src ('source/*.html')
+
+  .pipe (htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
+}
+
+//Scripts
+export const script = () => {
+  return gulp.src ('source/js/*.js')
+
+  .pipe(terser())
+  .pipe(gulp.dest('build/js'));
+}
+
+//Images
+export const optimizeImages = () => {
+  return gulp.src ('source/img/**/*.{jpg,png}')
+
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'));
+}
+
+const copyImages = () => {
+  return gulp.src ('source/img/**/*.{jpg,png}')
+  .pipe(gulp.dest('build/img'));
+}
+
+ //Webp
+export const createWebp = () => {
+  return gulp.src ('source/img/**/*.{jpg,png}')
+
+  .pipe(
+      squoosh({
+        webp: {}
+      })
+    )
+  .pipe(gulp.dest('build/img'));
+}
+
+//SVG
+export const svg = () => {
+  return gulp.src ('source/img/**/*.svg')
+
+  .pipe(svgo())
+  .pipe(gulp.dest('build/img'));
+}
+
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -41,5 +101,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  minify, styles, script, optimizeImages, copyImages, createWebp, svg, server, watcher
 );
